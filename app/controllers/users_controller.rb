6d0313805628b95e453f53377ce1109f5483a552
vagrant <vagrant@localhost.localdomain>
2020-08-22 12:55:39 +0000

    commit_2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,19 @@
 class UsersController < ApplicationController
   def index
+
+  	@users = User.all
+
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :introduction, :profile_image)
   end
 end
